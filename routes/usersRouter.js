4ad3d9b18a076d9c383d1354575ba936afe83fc3
Denso666 <Denisolf@mx.de>
2022-05-19 21:27:22 +0200

    add updateUser route and controller

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,14 +1,13 @@
 // import Router
 import { Router } from "express";
 // import getAllUsers
-import { getAllUsers, getSingleUser, createNewUser, deleteUser } from "../controllers/usersController.js";
+import { getAllUsers, getSingleUser, createNewUser, deleteUser, updateUser } from "../controllers/usersController.js";
 // declare userRouter
 const usersRouter = Router();
-// at the /api/users route we want to accept get & post 
-// to create a new user we need the post method and we want to have it at the generic main route
+// at the /api/users route we want to accept get & post (add the method and pass in the matching controller)
 usersRouter.route("/").get(getAllUsers).post(createNewUser);
 // at the /api/users/:id route we want to accept get, put & delete (add the method and pass in the matching controller)
-usersRouter.route("/:id").get(getSingleUser).delete(deleteUser);
+usersRouter.route("/:id").get(getSingleUser).delete(deleteUser).put(updateUser);
 
 // export Router
 export default usersRouter;
