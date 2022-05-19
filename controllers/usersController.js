843561ebac0529fae2b549a990e0caebbd93a02f
Denso666 <Denisolf@mx.de>
2022-05-19 08:21:11 +0200

    debugging

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,13 @@
 // users controller - we need data base query so we need to import pool
-import pool, { Pool } from "../db/pg.js";
+import pool from "../db/pg.js";
 
 // we have to export every controller seperately
 export const getAllUsers = (req, res) => {
-    pool.query("SELECT * FROM users").then(data => console.log(data)).catch(err => res.status(500).json(err));
+    pool
+        .query("SELECT * FROM users")
+        // here we say that our users is an object with the value of data.rows
+        .then((data) => res.status(200).json({ users: data.rows }))
+        //.then((data)=> console.log(data));
+        .catch((err) => res.status(500).json(err));
 };
+
