b555787aaef866e36b114b67c75cc5763c6d0faa
Denso666 <Denisolf@mx.de>
2022-05-19 13:20:24 +0200

    add createNewUser route and controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,26 +9,43 @@ export const getAllUsers = (req, res) => {
         .catch((err) => res.status(500).json(err));
 };
 
-// create a singleUser table
+// after adding the single User Route, create a singleUser controller
 export const getSingleUser = (req, res) => {
-  // if we only want one user we need to find a thing which
-  // lets us identify the user - that can be done via the ID
-  // the id can be found in the params!!!
-  // for example - console.log(req.params) -> response {id: 12}
-  // req.params.id
-  // destructure the id
-  const { id } = req.params;
-  pool
-    .query("SELECT * FROM users WHERE id=$1", [id])
-    .then((data) => {
-      if (data.rowCount == 0) {
-        res.status(404).send("There is no User matching this ID");
-      } else {
-        res.status(200).json(data.rows[0]);
-      }
-    })
-    .catch((err) => res.status(500).json(err));
-  // now test your :id route by using postman typing http://localhost:5000/api/users/2
-  // you should get the user with the id 2
-}
+    // if we only want one user we need to find a thing which
+    // lets us identify the user - that can be done via the ID
+    // the id can be found in the params!!!
+    // for example - console.log(req.params) -> response {id: 12}
+    // req.params.id
+    // destructure the id
+    const { id } = req.params;
+    pool
+        .query("SELECT * FROM users WHERE id=$1", [id])
+        .then((data) => {
+            if (data.rowCount == 0) {
+                res.status(404).send("There is no User matching this ID");
+            } else {
+                res.status(200).json(data.rows[0]);
+            }
+        })
+        .catch((err) => res.status(500).json(err));
+    // now test your :id route by using postman typing http://localhost:5000/api/users/2
+    // you should get the user with the id 2
+};
+// after adding post method to our getAllUserRoute and now we create the controller
+export const createNewUser = (req, res) => {
+    // as we know the data will be passed in the body
+    // to check if works console.log(req.body);
+    // deconstruct firstName and lastName
+    const { first_name, last_name } = req.body;
+    pool
+        .query(
+            // SQL - with INSERT into we add the firstname and last name(without) to the users table
+            // the $ parameter stands for the names - here $1 = first_name, $2 = last_name
+            "INSERT INTO users (first_name, last_name) VALUES ($1, $2) RETURNING *;",
+            [first_name, last_name]
+        )
+        .then((data) => res.status(201).json(data.rows[0]))
+            // to check if works console.log(data))
+        .catch((err) => res.status(500).json(err));
+};
 
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,11 +1,12 @@
 // import Router
 import { Router } from "express";
 // import getAllUsers
-import { getAllUsers, getSingleUser } from "../controllers/usersController.js";
+import { getAllUsers, getSingleUser, createNewUser } from "../controllers/usersController.js";
 // declare userRouter
 const usersRouter = Router();
 // at the /api/users route we want to accept get & post 
-usersRouter.route("/").get(getAllUsers);
+// to create a new user we need the post method and we want to have it at the generic main route
+usersRouter.route("/").get(getAllUsers).post(createNewUser);
 // at the /api/users/:id route we want to accept get, put & delete
 usersRouter.route("/:id").get(getSingleUser);
 
