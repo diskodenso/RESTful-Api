8ee721947424edf256cf9b123c8646fac7356d60
Denso666 <Denisolf@mx.de>
2022-05-22 22:55:49 +0200

    add createNewOrder and deleteOrder route and controllers

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -1,4 +1,5 @@
 // import pool from pg
+import query from 'express/lib/middleware/query';
 import pool from '../db/pg.js'
 
 // we have to export every controller seperately
@@ -19,6 +20,31 @@ export const getSingleOrder = (req, res) => {
                 res.status(404).send("There is no Order matching this ID");
             } else {
                 res.status(200).json(data.row[0]);
+            }
+        })
+        .catch((err) => res.status(500).json(err));
+};
+//------- create order controller ---------//
+export const createNewOrder = (req, res) => {
+    const { price, date, user_id } = req.body;
+    pool
+    query("INSERT INTO orders (price, date, user_id) VALUE ($1, $2, $3) REtURNING *;",
+        [price, date, user_id]
+    )
+        .then((data) => res.status(200).json(data.rows[0]))
+        .catch((err) => res.status(500).json(err));
+};
+//------- delete order controller ---------//
+export const deleteOrder = (req, res) => {
+    const { id } = req.params;
+    pool
+    query("DELETE user WHERE id=$1", [id]
+    )
+        .then((data) => {
+            if (data.rowCount == 0) {
+                res.status(404).send("There is no User matching this ID")
+            } else {
+                res.status(200).send("Order successfully deleted!")
             }
         })
         .catch((err) => res.status(500).json(err));
--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,12 +1,12 @@
 // import Router
 import { Router } from 'express';
-import { getAllOrders, getSingleOrder } from '../controllers/ordersController.js';
+import { createNewOrder, deleteOrder, getAllOrders, getSingleOrder } from '../controllers/ordersController.js';
 
 // call router function
 const ordersRouter = Router();
 // 1.  create Route for all orders(getAllOrders)
-ordersRouter.route("/").get(getAllOrders);
+ordersRouter.route("/").get(getAllOrders).post(createNewOrder);
 // 1. create Route for single order (getSingleOrder)
-ordersRouter.route("/:id").get(getSingleOrder);
+ordersRouter.route("/:id").get(getSingleOrder).delete(deleteOrder);
 // export router
 export default ordersRouter;
